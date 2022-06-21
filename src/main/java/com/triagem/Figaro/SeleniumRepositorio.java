b3f5d87f0d442c9198f778f396eb0b15bd467297
Marcos-balieiro <marcos.l.balieiro@gmail.com>
2022-06-21 14:27:53 -0300

    profile

--- src/main/java/com/triagem/Figaro/SeleniumRepositorio.java
+++ src/main/java/com/triagem/Figaro/SeleniumRepositorio.java
@@ -1,36 +1,54 @@
 package com.triagem.Figaro;
 
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.security.Principal;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import org.testng.annotations.Test;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
+
+
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 public class SeleniumRepositorio<usuario> {
-    private WebDriver driver;
+    public static WebDriver driver;
     private WebDriverWait wait;
 
     private long time = 15;
 
     @Test
-    public int login(int usuario) throws InterruptedException {
+    public int login(int usuario) throws InterruptedException, IOException {
         String url = "https://pje1g.trf1.jus.br/";
         System.setProperty("webdriver.gecko.driver", "GeckoDriver.exe");
-        driver = new FirefoxDriver();
+        Files.createDirectories(Paths.get("C:\\Temp"));
+        FirefoxProfile profile = new FirefoxProfile();
+        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+        profile.setPreference("browser.download.dir", "C:\\Temp"); // folder
+        profile.setPreference("browser.helperApps.neverAsk.saveToDisk", "application/pdf"); // MIME type
+        profile.setPreference("pdfjs.disabled", true); // disable the built-in viewer
+        profile.setPreference("browser.download.folderList", 2);
+        // profile.setPreference("browser.download.panel.shown", false);
+
+        capabilities.setCapability(FirefoxDriver.PROFILE, profile);
+        capabilities.setCapability(CapabilityType.ELEMENT_SCROLL_BEHAVIOR, 1);
+        final WebDriver driver = new FirefoxDriver(capabilities);
+
+        wait = new WebDriverWait(driver, 15);
+
         driver.get(url);
         this.time = 15000;
         wait = new WebDriverWait(driver, Duration.ofSeconds(time));
@@ -51,7 +69,9 @@ public class SeleniumRepositorio<usuario> {
     return 1; }
     public void teste2() throws InterruptedException {
         driver.navigate().to("https://pje1g.trf1.jus.br/pje/Processo/ConsultaProcesso/listView.seam");
-
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+        String janelapadrao = driver.getWindowHandle();
         LocalDateTime hoje = LocalDateTime.now();
         String iddatahoje = "fPP:dataAutuacaoDecoration:dataAutuacaoFimInputDate";
         String iddatasexta = "fPP:dataAutuacaoDecoration:dataAutuacaoInicioInputDate";
@@ -76,24 +96,33 @@ public class SeleniumRepositorio<usuario> {
         String pesquisar="fPP:searchProcessos";
         driver.findElement(By.id(pesquisar)).click();
 
-
         wait.until(ExpectedConditions.presenceOfElementLocated(By.id(pesquisar)));
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div[6]/div/div/div/div[2]/form/div[2]/div/table/tbody/tr[1]/td[1]")));
         driver.findElement(By.xpath("/html/body/div[6]/div/div/div/div[2]/form/div[2]/div/table/tbody/tr[1]/td[1]")).click();
 
         Thread.sleep(4000);
-        String spam="/html/body/div[2]/div[2]/div[2]/table/tbody/tr[2]/td/table/tbody/tr/td/form[1]/div[2]/div[1]/div[5]/div[2]/div[1]/ul/li[1]/a/span";
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(spam)));
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(spam)));
-        driver.findElement(By.xpath(spam)).click();
+        Set<String> janela = driver.getWindowHandles();
+        for (String handle:janela) {
+            if (!handle.equals(janelapadrao)) {
+                driver.switchTo().window(handle);
 
-        String download = "/html/body/div[2]/div[2]/div[2]/table/tbody/tr[2]/td/table/tbody/tr/td/form[2]/div[3]/div[2]/div[3]/ul/li[3]/a/i";
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(download)));
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(download)));
-        driver.findElement(By.xpath(download)).click();
-        //driver.findElement(By.xpath(download)).sendKeys(Keys.ENTER);
+                String spam = "/html/body/div[2]/div[2]/div[2]/table/tbody/tr[2]/td/table/tbody/tr/td/form[2]/div[3]/div[2]/div[2]/ul/li[1]/a";
+                Thread.sleep(2000);
+                driver.findElement(By.xpath(spam)).click();
+                String spam2 = "/html/body/div[2]/div[2]/div[2]/table/tbody/tr[2]/td/table/tbody/tr/td/form[2]/div[3]/div[2]/div[2]/ul/li[4]/a";
+                Thread.sleep(2000);
+                driver.findElement(By.xpath(spam2)).click();
 
+                String download = "/html/body/div[2]/div[2]/div[2]/table/tbody/tr[2]/td/table/tbody/tr/td/form[2]/div[3]/div[2]/div[3]/ul/li[3]/a";
+                wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(download)));
+                wait.until(ExpectedConditions.elementToBeClickable(By.xpath(download)));
+                //Thread.sleep(4000);
+                driver.findElement(By.xpath(download)).click();
+                Actions action = new Actions(driver);
+                action.sendKeys(Keys.chord(Keys.ENTER));
 
+            }
+        }
     }
     public String data()  {
         LocalDateTime hoje = LocalDateTime.now();
