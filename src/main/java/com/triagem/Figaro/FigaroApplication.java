ad6e76d0030ac185d8d369861337c09a8eb3dfe7
Marcos-balieiro <marcos.l.balieiro@gmail.com>
2022-06-24 17:45:24 -0300

    Formulario com dowload

--- src/main/java/com/triagem/Figaro/FigaroApplication.java
+++ src/main/java/com/triagem/Figaro/FigaroApplication.java
@@ -3,10 +3,12 @@ package com.triagem.Figaro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.awt.*;
+
 @SpringBootApplication
 public class FigaroApplication {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, AWTException {
 		SpringApplication.run(FigaroApplication.class, args);
 
 		SeleniumRepositorio login = new SeleniumRepositorio();
