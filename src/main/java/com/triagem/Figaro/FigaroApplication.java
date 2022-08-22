1f6da6f04ed11cdcd2936f7c317c1ae6aeb96d5d
Matheus Martins <matheus.martins999.mm@gmail.com>
2022-08-22 16:59:49 -0300

    criação BancoController e ControllerEntidade

--- src/main/java/com/triagem/Figaro/FigaroApplication.java
+++ src/main/java/com/triagem/Figaro/FigaroApplication.java
@@ -1,8 +1,11 @@
 package com.triagem.Figaro;
 
+import modelo.Entidade;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import repository.SeleniumRepositorio;
+import controller.BancoController;
+import modelo.Entidade;
 
 import java.awt.*;
 
@@ -12,9 +15,13 @@ public class FigaroApplication {
 	public static void main(String[] args) throws InterruptedException, AWTException {
 		SpringApplication.run(FigaroApplication.class, args);
 
-		SeleniumRepositorio login = new SeleniumRepositorio();
+		//SeleniumRepositorio login = new SeleniumRepositorio();
+		//int usuario = 0;
+		//login.login(usuario);
+		Entidade entidade = new Entidade();
+		BancoController bancoController = new BancoController();
+		bancoController.dadosEntidade();
+		System.out.println(entidade.getNome());
 
-		int usuario = 0;
-		login.login(usuario);
 	}
 }
