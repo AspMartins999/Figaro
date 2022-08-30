aaccafd8317d2d794b6dfc2beb4788e1e3e5cc3e
Marcos-balieiro <marcos.l.balieiro@gmail.com>
2022-08-30 11:48:51 -0300

    atualização

--- src/main/java/DAO/DAOEntidade.java
+++ src/main/java/DAO/DAOEntidade.java
@@ -55,4 +55,5 @@ public class DAOEntidade extends ConexaoSQLite {
         this.desconectar();
         return true;
     }
+
 }
--- src/main/java/com/triagem/Figaro/FigaroApplication.java
+++ src/main/java/com/triagem/Figaro/FigaroApplication.java
@@ -1,20 +1,25 @@
 package com.triagem.Figaro;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import repository.SeleniumRepositorio;
 
 import java.awt.*;
+import java.sql.*;
+
+import repository.SeleniumRepositorio;
 
 @SpringBootApplication
 public class FigaroApplication {
 
-	public static void main(String[] args) throws InterruptedException, AWTException {
+	public static void main(String[] args) throws InterruptedException, AWTException, ClassNotFoundException, SQLException {
 		SpringApplication.run(FigaroApplication.class, args);
 
 		SeleniumRepositorio login = new SeleniumRepositorio();
 
 		int usuario = 0;
 		login.login(usuario);
+
 	}
+
 }
